369d736b5670c1c076d01fa00296b3619983e89e
RunninPekkin <erhembaatardashdorj@gmail.com>
2019-12-08 16:19:31 +0000

    Calc class: added calulateFinalTemperature, calculateHeatLoss, and calculateFinalMass

--- src/main/java/physics/project/Calc.java
+++ src/main/java/physics/project/Calc.java
@@ -1,4 +1,22 @@
 package physics.project;
 
 public class Calc {
+	public static double calculateFinalTemperature(double mass1, double mass2, double specificHeat1, double specificHeat2, double temperature1 , double temperature2)
+	{
+		double numerator = (mass1*specicHeat1*temperature1)+(mass2*specificHeat2*temperature2);
+		double denominator = (mass1*specificHeat1)+(mass2*specificHeat2);
+		return (numerator/denominator);
+	}
+
+	public static double calculateEnergyLoss(temperature1, temperature2)
+	{
+		return Math.abs(temperature1-temperature2);
+	}
+	
+	public static double calculateFinalMass(mass1, mass2)
+	{
+		return mass1+mass2;
+	}
+
+
 }
